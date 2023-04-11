ff58bd34335da2deee649275625cc47ade0ea5b9
emimo43 <e.mimo.lueiza@gmail.com>
2023-04-10 23:22:00 -0400

    Operadores en C

---
+++ ATLAcademy/OperadoresSimplificados.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1 = 10;
+    int num2 = 5;
+    // suma +
+    // num1 = num1 + 7; //17
+
+   //num1 += 7; //17
+    //printf("%d",num1);
+    //printf("\n");
+
+    num1 -= num2;
+    printf("%d",num1);
+    
+
+    return 0;
+}
